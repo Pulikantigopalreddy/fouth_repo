dea0aea486fc3322967e9293ef8c5351e585fc15
Pulikantigopalreddy <reddypulikanti123@gmail.com>
2025-03-20 17:07:49 +0530

    Create application.py

---
+++ application.py
@@ -0,0 +1,22 @@
+def add(x, y):
+  return x + y
+
+print(add(20, 30)
+
+
+def sub(x, y):
+  return x - y
+
+print(sub(30, 40)
+
+
+def mul(x, y):
+  return x * y
+
+print(mul(30, 30)
+
+
+def div(x, y):
+  return x / y
+
+print(div(30, 30)
