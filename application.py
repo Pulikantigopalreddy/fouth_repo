7aefda8c8763745eb793d73b61fb0402121b7f63
gopalreddypulikanti <gopalpulikanti33@gmail.com>
2025-03-20 17:50:48 +0530

    new file added

--- application.py
+++ application.py
@@ -1,22 +1,25 @@
 def add(x, y):
   return x + y
 
-print(add(20, 30)
+print(add(20, 30))
 
 
 def sub(x, y):
   return x - y
 
-print(sub(30, 40)
+print(sub(30, 40))
 
 
 def mul(x, y):
   return x * y
 
-print(mul(30, 30)
+print(mul(30, 30))
 
 
 def div(x, y):
   return x / y
 
-print(div(30, 30)
+print(div(30, 30))
+
+
+print("hello")
